1c092f49a2efb6d6f7f800319e4498cdf8bb36d9
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-09-26 15:46:56 +0200

    added server.py

--- FIAT_GUI/app.py
+++ FIAT_GUI/app.py
@@ -18,4 +18,4 @@ app = Dash(
 app.layout = layout
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
---
+++ FIAT_GUI/server.py
@@ -0,0 +1,18 @@
+"""Script for serving the app locally."""
+
+import webbrowser
+from threading import Timer
+
+from waitress import serve
+
+from FIAT_GUI import app
+
+
+def open_browser() -> None:
+    """Open browser to localhost."""
+    webbrowser.open_new("http://127.0.0.1:8080/")
+
+
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+    serve(app.server)
